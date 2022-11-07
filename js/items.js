1f1e75bc7e60b21983ab4173a031175174da9273
Tiffany Ngo <tifngo99@gmail.com>
2022-11-07 13:36:56 -0800

    second commit for task5.

--- js/items.js
+++ js/items.js
@@ -1,6 +1,7 @@
 // Initialize a new TaskManager with currentId set to 0
-const itemsController = new ItemsController(0);
+// import items from "./js/items.js"; 
 
+const itemsController = new ItemsController(0);
 
 function addItemCard(item){
     const itemHTML = '<div class="card" style="width: 20rem;">\n' +
@@ -18,23 +19,29 @@ function addItemCard(item){
 
 function loadStorageSampleData(){
     if(!localStorage.getItem("items")){
-        const sampleItems = [{'name':'juice',
+        const sampleItems = [{'name':'Sweater',
         'img':'https://www.gs1india.org/media/Juice_pack.jpg',
-        'description':'Orange and Apple juice fresh and delicious'},
-        {'name':'Ruffles Chicken',
+        'description':'A soft cotton sweater'},
+        {'name':'Sweater',
         'img':'https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/13086598_LXL1.jpg',
-        'description':'Ruffles Potato Chips - Chicken'}];
+        'description':'Sweater'}];
         localStorage.setItem("items", JSON.stringify(sampleItems));
     }
 }
 
 function loadCardsListFromItemsController(){
+    console.log("load card list from items controller called");
     for(var i = 0, size = itemsController.items.length; i < size ; i++){
         const item = itemsController.items[i];
         addItemCard(item);
+        console.log(item);
     }
 }
 
 loadStorageSampleData();
 itemsController.loadItemsFromLocalStorage();
 loadCardsListFromItemsController();
+
+console.log(itemsController);
+
+// export default items;
--- js/itemsController.js
+++ js/itemsController.js
@@ -1,26 +1,42 @@
+// import items from "./js/items.js"; 
+
 class ItemsController {
     constructor(currentId = 0) {
         this.items = []; 
         this.currentId = currentId;
     }
 
-
-    addItem(name, description, img, created) {
+    addItem(name, description, img) {
         const item = {
             id: this.currentId++, 
             name: name, 
             description: description,
             img: img,
-            created: created,
-        };
-        
+            // sizes: sizes,
+            // colors: colors,
+            // created: created,
+        };        
         this.items.push(item);
     };
     
+    loadItemsFromLocalStorage() {
+        const unparsedLSItems = localStorage.getItem('items');
+        const parsedLSItems = JSON.parse(unparsedLSItems);
+        for(let i = 0; i<parsedLSItems.length; i++){
+            console.log(parsedLSItems[i]);
+            this.addItem(parsedLSItems[i].name, parsedLSItems[i].description, parsedLSItems[i].img);
+        }
+    };
+
+    saveItemsToLocalStorage() {
+        const save = localStorage.setItem();
+    }
 } 
 
-
-
 // const newItem = new ItemsController()
-// newItem.addItem("shoes", "footwear", "pictures", "")
+// newItem.addItem("shoes", "footwear", "pictures");
 // console.log(newItem.items);
+
+console.log("hello world");
+
+// export default items;
